81d52157217be74d6533cf8fa496720ed656bebd
XenosWarlocks <139984318+XenosWarlocks@users.noreply.github.com>
2024-05-18 05:18:58 +0530

    Add files via upload

--- db/queries.ts
+++ db/queries.ts
@@ -40,12 +40,14 @@ export const getUnits = cache(async () => {
 
     const data = await db.query.units.findMany({
         where: eq(units.courseId, userProgress.activeCourseId),
+        orderBy: (units, { asc }) => [asc(units.order)],
         with: {
             lessons: {
+                orderBy: (lessons, { asc }) => [asc(lessons.order)],
                 with: {
                     challenges: {
+                        orderBy: (challenges, { asc }) => [asc(challenges.order)],
                         with: {
-                            challengeOptions: true,
                             challengeProgress: {
                                 where: eq(challengeProgress.userId, userId),
                             },
@@ -85,6 +87,16 @@ export const getCourses = cache(async () => {
 export const getCourseById = cache(async (courseId: number) => {
     const data = await db.query.courses.findFirst({
         where: eq(courses.id, courseId),
+        with: {
+            units: {
+                orderBy: (units, { asc }) => [asc(units.order)],
+                with: {
+                    lessons: {
+                        orderBy: (lessons, { asc }) => [asc(lessons.order)]
+                    }
+                }
+            }
+        }
     });
 
     return data;
